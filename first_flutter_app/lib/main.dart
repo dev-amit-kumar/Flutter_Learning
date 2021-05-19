50bc7d409922d10d8d734292c2b5a8a866c00e6a
Amit Kumar <amitkumar.developer1@gmail.com>
2021-05-19 19:17:30 +0530

    added next and back button (not working due to stateless widget)

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -7,8 +7,71 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  int quesIndex = 0;
+  var questionList = [
+    {
+      'ques': 'What is your favourite color?',
+      'ans1': 'Pink',
+      'ans2': 'Black',
+      'ans3': 'Blue',
+      'ans4': 'White'
+    },
+    {
+      'ques': 'What is your favourite animal?',
+      'ans1': 'Dog',
+      'ans2': 'Cat',
+      'ans3': 'Lion',
+      'ans4': 'Tiger'
+    },
+    {
+      'ques': 'What is your favourite animal?',
+      'ans1': 'Dog',
+      'ans2': 'Cat',
+      'ans3': 'Lion',
+      'ans4': 'Tiger'
+    },
+    {
+      'ques': 'What is your favourite animal?',
+      'ans1': 'Dog',
+      'ans2': 'Cat',
+      'ans3': 'Lion',
+      'ans4': 'Tiger'
+    },
+    {
+      'ques': 'What is your favourite animal?',
+      'ans1': 'Dog',
+      'ans2': 'Cat',
+      'ans3': 'Lion',
+      'ans4': 'Tiger'
+    },
+    {
+      'ques': 'What is your favourite animal?',
+      'ans1': 'Dog',
+      'ans2': 'Cat',
+      'ans3': 'Lion',
+      'ans4': 'Tiger'
+    }
+  ];
   void ansQuestion(int val) {
     print(val);
+  }
+
+  void goBack() {
+    if (quesIndex > 0) {
+      quesIndex -= 1;
+      print(quesIndex);
+    } else {
+      print('${quesIndex} First question');
+    }
+  }
+
+  void goNext() {
+    if (quesIndex < questionList.length - 1) {
+      quesIndex += 1;
+      print(quesIndex);
+    } else {
+      print('${quesIndex} Last question');
+    }
   }
 
   @override
@@ -20,13 +83,26 @@ class MyApp extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text('Question 1'),
-          ElevatedButton(child: Text('a'), onPressed: () => ansQuestion(1)),
-          ElevatedButton(child: Text('b'), onPressed: () => ansQuestion(2)),
-          ElevatedButton(child: Text('c'), onPressed: () => ansQuestion(3)),
-          ElevatedButton(child: Text('d'), onPressed: () => ansQuestion(4)),
+          Row(children: [
+            Text('Q${quesIndex + 1}. '),
+            Text(questionList[quesIndex]['ques'])
+          ]),
           ElevatedButton(
-              child: Text('Clear'), onPressed: () => print('Cleared')),
+              child: Text(questionList[quesIndex]['ans1']),
+              onPressed: () => ansQuestion(1)),
+          ElevatedButton(
+              child: Text(questionList[quesIndex]['ans2']),
+              onPressed: () => ansQuestion(2)),
+          ElevatedButton(
+              child: Text(questionList[quesIndex]['ans3']),
+              onPressed: () => ansQuestion(3)),
+          ElevatedButton(
+              child: Text(questionList[quesIndex]['ans4']),
+              onPressed: () => ansQuestion(4)),
+          Row(children: [
+            ElevatedButton(child: Text('Back'), onPressed: goBack),
+            ElevatedButton(child: Text('Next'), onPressed: goNext)
+          ])
         ],
       ),
     ));
