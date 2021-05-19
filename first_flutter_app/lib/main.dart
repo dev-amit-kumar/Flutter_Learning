2e0e9085a12db4ef2a9954351c55563188469a9e
Amit Kumar <amitkumar.developer1@gmail.com>
2021-05-19 18:36:55 +0530

    added simple button

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -6,16 +6,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  void ansQuestion(int val) {
+    print(val);
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('This is the body'),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Column(
+        children: [
+          Text('Question 1'),
+          ElevatedButton(child: Text('a'), onPressed: () => ansQuestion(1)),
+          ElevatedButton(child: Text('b'), onPressed: () => ansQuestion(2)),
+          ElevatedButton(child: Text('c'), onPressed: () => ansQuestion(3)),
+          ElevatedButton(child: Text('d'), onPressed: () => ansQuestion(4)),
+          ElevatedButton(
+              child: Text('Clear'), onPressed: () => print('Cleared')),
+        ],
+      ),
+    ));
   }
 }
