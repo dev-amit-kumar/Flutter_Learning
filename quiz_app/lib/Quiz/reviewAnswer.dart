c1bce1ddadfb3041624018464ab3e83958730c74
Amit Kumar <amitkumar.developer1@gmail.com>
2021-05-24 14:14:14 +0530

    scrollable review screen

--- quiz_app/lib/Quiz/reviewAnswer.dart
+++ quiz_app/lib/Quiz/reviewAnswer.dart
@@ -6,53 +6,62 @@ class Result extends StatelessWidget {
   Result({this.questionList, this.answerList});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5),
-      child: Column(
-        children: [
-          Text(
-            'Review Answers',
-            style: TextStyle(fontWeight: FontWeight.w800, fontSize: 22),
-          ),
-          ...(questionList as List).map((question) {
-            return Card(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text('Ques: ',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 14)),
-                        Flexible(
-                          child: Text(
-                            question['ques'],
-                            style: TextStyle(fontSize: 15),
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Text(
+              'Review Answers',
+              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 22),
+            ),
+            ...(questionList as List).map((question) {
+              return Card(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Ques: ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w800, fontSize: 14)),
+                          Flexible(
+                            child: Text(
+                              question['ques'],
+                              style: TextStyle(fontSize: 15),
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('Ans:   ',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 14)),
-                        Flexible(
-                          child: Text(
-                            answerList[question['quesId']],
-                            style: TextStyle(fontSize: 15),
+                          IconButton(
+                            onPressed: null,
+                            icon: const Icon(Icons.create_rounded),
+                            tooltip: 'Edit answer',
+                            iconSize: 16,
                           ),
-                        )
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('Ans:   ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w800, fontSize: 14)),
+                          Flexible(
+                            child: Text(
+                              answerList[question['quesId']],
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          }).toList(),
-          ElevatedButton(onPressed: null, child: Text('Submit'))
-        ],
+              );
+            }).toList(),
+            ElevatedButton(onPressed: null, child: Text('Submit'))
+          ],
+        ),
       ),
     );
   }
